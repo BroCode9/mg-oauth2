c4d013fa1f2ae9c0c4d788e6b29d33c34a42dab0
Darko Bacic <darkob@backbase.com>
2018-12-07 14:49:13 +0100

    - Added Swift & kotlin

--- ios/Classes/MgOauth2Plugin.m
+++ ios/Classes/MgOauth2Plugin.m
@@ -1,20 +1,8 @@
 #import "MgOauth2Plugin.h"
+#import <mg_oauth2/mg_oauth2-Swift.h>
 
 @implementation MgOauth2Plugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"mg_oauth2"
-            binaryMessenger:[registrar messenger]];
-  MgOauth2Plugin* instance = [[MgOauth2Plugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+  [SwiftMgOauth2Plugin registerWithRegistrar:registrar];
 }
-
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
-}
-
 @end
