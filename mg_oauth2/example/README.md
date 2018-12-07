e95e19954ca23a263f0d16375399d172cd0bd54c
Darko Bacic <darkob@backbase.com>
2018-12-07 15:12:10 +0100

    - removed auto generated folder

--- mg_oauth2/example/README.md
+++
@@ -1,8 +0,0 @@
-# mg_oauth2_example
-
-Demonstrates how to use the mg_oauth2 plugin.
-
-## Getting Started
-
-For help getting started with Flutter, view our online
-[documentation](https://flutter.io/).
--- mg_oauth2/ios/Classes/MgOauth2Plugin.m
+++
@@ -1,8 +0,0 @@
-#import "MgOauth2Plugin.h"
-#import <mg_oauth2/mg_oauth2-Swift.h>
-
-@implementation MgOauth2Plugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftMgOauth2Plugin registerWithRegistrar:registrar];
-}
-@end
