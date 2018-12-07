5b6c0fc4f38dcd3e5e894c791cb5101d8fcd0298
Darko Bacic <darkob@backbase.com>
2018-12-07 14:41:57 +0100

    - clearing project

--- ios/Classes/MgOatuh2Plugin.m
+++
@@ -1,8 +0,0 @@
-#import "MgOatuh2Plugin.h"
-#import <mg_oatuh2/mg_oatuh2-Swift.h>
-
-@implementation MgOatuh2Plugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftMgOatuh2Plugin registerWithRegistrar:registrar];
-}
-@end
