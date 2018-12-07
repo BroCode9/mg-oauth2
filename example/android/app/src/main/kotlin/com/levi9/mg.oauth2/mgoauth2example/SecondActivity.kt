5efc5fc3c42d87308eaf2c27fd98948bd4b4c039
Darko Bacic <darkob@backbase.com>
2018-12-07 22:29:54 +0100

    - added request for sign in and parsing the code

--- example/android/app/src/main/kotlin/com/levi9/mg.oauth2/mgoauth2example/SecondActivity.kt
+++ example/android/app/src/main/kotlin/com/levi9/mg.oauth2/mgoauth2example/SecondActivity.kt
@@ -2,8 +2,10 @@ package com.levi9.mg.oauth2.mgoauth2example
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import android.widget.EditText
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class SecondActivity : AppCompatActivity() {
 
@@ -11,11 +13,29 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        val btnSubmit = findViewById<Button>(R.id.btnSubmit)
-        btnSubmit.setOnClickListener {
-            val etText = findViewById<EditText>(R.id.etText)
-            MainActivity.calResult.success(etText.text.toString())
-            finish()
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = MyWebClient(this)
+
+        webView.loadUrl("https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=eeffec03-c281-4980-b6c0-8c5cbb564dc4&response_type=code&redirect_uri=https://login.microsoftonline.com/common/oauth2/nativeclient&response_mode=query&scope=offline_access%20user.read%20mail.read&state=12345")
+    }
+}
+
+class MyWebClient(val activity: AppCompatActivity) : WebViewClient() {
+
+    override fun onPageFinished(view: WebView?, url: String?) {
+        super.onPageFinished(view, url)
+    }
+
+    override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+        if(url != null && url.contains("code")) {
+            val start = url.indexOf("?code") - 5
+            val end = url.indexOf("&state")
+            MainActivity.calResult.success(url.substring(start, end))
+            activity.finish()
+            return true
         }
+
+        return super.shouldOverrideUrlLoading(view, url)
     }
 }
